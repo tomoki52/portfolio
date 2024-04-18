584682a03c24f1038f5ee9fc0d135ab43b4a437e
tomoki52 <konishi.tomoki.ap@alumni.tsukuba.ac.jp>
2024-04-19 08:14:08 +0900

    CSSまとめた

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,69 +1,62 @@
 export default function Home() {
   return (
     <main>
-      <section className="text-gray-600 body-font">
-        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-              小西智樹
-              <br />
-              Tomoki Konishi
-            </h1>
-            <p className="mb-8 leading-relaxed">筑波大学大学院</p>
-          </div>
-          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-            <img
-              className="object-cover object-center rounded"
-              alt="hero"
-              src="https://dummyimage.com/720x600"
-            />
-          </div>
-        </div>
-      </section>
+      <Top />
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
-          <div className="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
-            <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
-              <h2 className="text-gray-900 text-lg title-font font-medium mb-2">
-                Publications
-              </h2>
+          <div className="line" />
+          <div className="line">
+            <div className="list-container">
+              <h2 className="list-heading">Publications</h2>
               <p className="leading-relaxed text-base">投稿論文</p>
             </div>
           </div>
-          <div className="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
-            <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
-              <h2 className="text-gray-900 text-lg title-font font-medium mb-2">
-                Publications
-              </h2>
-              <p className="leading-relaxed text-base">投稿論文</p>
-            </div>
-          </div>
-          <div className="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
-            <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
-              <h2 className="text-gray-900 text-lg title-font font-medium mb-2">
-                Achivement
-              </h2>
+          <div className="line">
+            <div className="list-container">
+              <h2 className="list-heading">Achivements</h2>
               <p className="leading-relaxed text-base">ハッカソン</p>
             </div>
           </div>
-          <div className="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
-            <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
-              <h2 className="text-gray-900 text-lg title-font font-medium mb-2">
-                Experiences
-              </h2>
-              <p className="leading-relaxed text-base">アルバイト</p>
+          <div className="line">
+            <div className="list-container">
+              <h2 className="list-heading">Experiences</h2>
+              <p className="leading-relaxed text-base">
+                アルバイト，インターン
+              </p>
             </div>
           </div>
-          <div className="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
-            <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
-              <h2 className="text-gray-900 text-lg title-font font-medium mb-2">
-                Works
-              </h2>
+          <div className="line">
+            <div className="list-container">
+              <h2 className="list-heading">Works</h2>
               <p className="leading-relaxed text-base">色々</p>
             </div>
           </div>
         </div>
       </section>
     </main>
+  );
+}
+
+function Top() {
+  return (
+    <section className="text-gray-600 body-font">
+      <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+            小西智樹
+            <br />
+            Tomoki Konishi
+          </h1>
+          <p className="mb-8 leading-relaxed">筑波大学大学院</p>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center rounded"
+            alt="hero"
+            src="https://dummyimage.com/720x600"
+          />
+        </div>
+      </div>
+    </section>
   );
 }
