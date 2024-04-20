fb6e9c326dc2d7baf1741ec60e06b51c0581c2c2
tomoki52 <konishi.tomoki.ap@alumni.tsukuba.ac.jp>
2024-04-21 00:03:55 +0900

    [WIP]CSSまとめる

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,92 @@
+import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <Top />
-      <section className="text-gray-600 body-font">
-        <div className="container px-5 py-24 mx-auto">
-          <div className="line" />
-          <div className="line">
+      <div className="container md:px-40 mx-auto flex-col">
+        <Top />
+        <section className="text-gray-600 body-font">
+          <div className="container px-5 py-24 mx-auto">
+            <div className="line" />
             <div className="list-container">
-              <h2 className="list-heading">Publications</h2>
-              <p className="leading-relaxed text-base">投稿論文</p>
+              <h2 className="section-title">About</h2>
+              <ul className="list-disc pl-5 text-xl leading-relaxed">
+                <li>
+                  <p>
+                    筑波大学 理工情報生命学術院 システム情報工学研究群
+                    情報理工学位プログラム
+                  </p>
+                  <p>博士前期課程2年</p>
+                </li>
+                <li>
+                  <p>インタラクティブプログラミング研究室所属</p>
+                </li>
+              </ul>
+            </div>
+            <div className="line" />
+            <div className="list-container">
+              <h2 className="section-title">Publications</h2>
+              <h3 className="section-subtitle">国内学会（査読なし）</h3>
+              <ul className="list-disc ml-5 text-xl leading-relaxed">
+                <li className="mb-8">
+                  <p className="font-bold">
+                    アクティブ音響センシングを用いた手指の関節角度推定
+                  </p>
+                  <p>
+                    <span className="underline">小西智樹</span>，
+                    角田陸，國分晴利，久保勇貴，志築文太郎．
+                    情報処理学会研究報告，Vol.2024-HCI-201，No.21，1月16-17日，
+                    2023，沖縄，情報処理学会，8pages．
+                    <p>
+                      link: &nbsp;
+                      <Link
+                        href="http://id.nii.ac.jp/1001/00223223/"
+                        className="underline"
+                      >
+                        http://id.nii.ac.jp/1001/00223223/
+                      </Link>
+                    </p>
+                  </p>
+                </li>
+                <li className="mb-8">
+                  <p className="font-bold">
+                    VR環境におけるフリック入力を利用した低オクルージョンなQWERTYキーボードの実装
+                  </p>
+                  <p>
+                    折居篤，
+                    <span className="underline">小西智樹</span>， 志築文太郎．
+                    第31回インタラクティブシステムとソフトウェアに関するワークショップ（WISS2023），
+                    11月29-12月1日，2023，山梨，日本ソフトウェア科学会，
+                    3pages．
+                  </p>
+                </li>
+                <li className="mb-8">
+                  <p className="font-bold">
+                    VR環境におけるフリック入力を利用した低オクルージョンなQWERTYキーボードの実装
+                  </p>
+                  <p>
+                    佐藤玲生，
+                    <span className="underline">小西智樹</span>，志築文太郎．
+                    第31回インタラクティブシステムとソフトウェアに関するワークショップ（WISS2023），
+                    11月29-12月1日，2023，山梨，日本ソフトウェア科学会，
+                    3pages．
+                  </p>
+                </li>
+                <li className="mb-8">
+                  <p className="font-bold">
+                    スマートフォンを用いたAR環境における片手用空中キーボードの性能調査
+                  </p>
+                  <p>
+                    佐藤玲生，
+                    <span className="underline">小西智樹</span>，志築文太郎．
+                    情報処理学会研究報告，Vol.2024-HCI-206，No.4，
+                    1月15-16日，2024，沖縄，情報処理学会，8pages．
+                  </p>
+                </li>
+              </ul>
             </div>
           </div>
-          <div className="line">
-            <div className="list-container">
-              <h2 className="list-heading">Achivements</h2>
-              <p className="leading-relaxed text-base">ハッカソン</p>
-            </div>
-          </div>
-          <div className="line">
-            <div className="list-container">
-              <h2 className="list-heading">Experiences</h2>
-              <p className="leading-relaxed text-base">
-                アルバイト，インターン
-              </p>
-            </div>
-          </div>
-          <div className="line">
-            <div className="list-container">
-              <h2 className="list-heading">Works</h2>
-              <p className="leading-relaxed text-base">色々</p>
-            </div>
-          </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </main>
   );
 }
@@ -47,7 +101,6 @@ function Top() {
             <br />
             Tomoki Konishi
           </h1>
-          <p className="mb-8 leading-relaxed">筑波大学大学院</p>
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
           <img
